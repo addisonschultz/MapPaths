a5598b36887a8a270886a689c56c8d764dfc1672
Addison <addison@framer.com>
2019-11-14 11:29:57 +0100

    hook up geocoding and directions api

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
 import "./App.css";
 
-// import Mapbox from "./components/Mapbox";
+import Mapbox from "./components/Mapbox";
 // import InputLocation1 from "./components/InputLocation1";
 // import InputLocation2 from "./components/InputLocation2";
 
+import * as GeoData from "./data/directions.json";
+
 function App() {
   const [location1, setLocation1] = React.useState(
-    "Singel 258, Amsterdam 1016AB"
+    "Albert Cuypstraat, 1073 BD Amsterdam"
   );
   const [location2, setLocation2] = React.useState(
     "Mercatorplein 72, Amsterdam 1056CL"
   );
+
+  const [points, setPoints] = React.useState([]);
 
   const handleClick = async () => {
     const location1Response = await fetch(
@@ -30,20 +34,26 @@ function App() {
     const location2Data = await location2Response.json();
     const location2Coords = location2Data.features[0].center;
 
-    console.log("LOC1", location1Coords);
-    console.log("LOC2", location2Coords);
+    console.log("LOC1 Coordinates", location1Coords);
+    console.log("LOC2 Coordinates", location2Coords);
 
     const directions = await fetch(
-      `https://api.mapbox.com/directions/v5/mapbox/driving/${location1Coords[0]},${location1Coords[1]};${location2Coords[0]},${location2Coords[1]}\\?access_token=pk.eyJ1IjoiYWRkaXNvbnNjaHVsdHoiLCJhIjoiY2p1YXpsMWxxMDBvejQ0cGRqNm5yZDh4aSJ9.-nkTtj1EUib3G4L1oqMaTQ
+      `https://api.mapbox.com/directions/v5/mapbox/walking/${location1Coords[0]},${location1Coords[1]};${location2Coords[0]},${location2Coords[1]}\\?geometries=geojson&access_token=pk.eyJ1IjoiYWRkaXNvbnNjaHVsdHoiLCJhIjoiY2p1YXpsMWxxMDBvejQ0cGRqNm5yZDh4aSJ9.-nkTtj1EUib3G4L1oqMaTQ
       `
     );
-    const routes = await directions.json();
-    console.log("FINAL ROUTE", routes);
+    const response = await directions.json();
+
+    const points = response.routes[0].geometry.coordinates;
+    console.log("POINTS", points);
+
+    setPoints([...points]);
   };
+
+  function Animate() {}
 
   return (
     <div className="App">
-      <div id="inputWrapper" style={{ zIndex: 200 }}>
+      <div id="inputWrapper" style={{ zIndex: 1000, paddingTop: 50 }}>
         <input
           placeholder={location1}
           onChange={e => {
@@ -61,9 +71,22 @@ function App() {
 
         <button onClick={handleClick}>Submit</button>
       </div>
-      {/* <div>
-        <Mapbox />
+      {/* <div
+        style={{ position: "absolute", top: 150 }}
+        className={"mapContainer"}
+      >
+        <Mapbox points={points} />
       </div> */}
+      <div
+        style={{ position: "absolute", top: 150 }}
+        className={"mapContainer"}
+      >
+        <Mapbox
+          points={GeoData.default[0].legs[0].steps[2].intersections.map(
+            x => x.location
+          )}
+        />
+      </div>
     </div>
   );
 }
--- src/components/Mapbox.jsx
+++ src/components/Mapbox.jsx
@@ -7,32 +7,9 @@ import { PathLayer } from "@deck.gl/layers";
 import * as GeoData from "../data/directions.json";
 
 function Mapbox(props) {
-  // console.log(
-  //   GeoData.default[0].legs[0].steps.map(step =>
-  //     console.log(step.intersections)
-  //   )
-  // );
-
-  console.log(
-    GeoData.default[0].legs[0].steps[2].intersections.map(x => x.location)
-  );
-
   const data = [
     {
-      // path: [
-      //   [-74.00578, 40.713067],
-      //   [-74.004577, 40.712425],
-      //   [-74.003626, 40.71365],
-      //   [-74.002666, 40.714243],
-      //   [-74.002136, 40.715177],
-      //   [-73.998493, 40.713452],
-      //   [-73.997981, 40.713673],
-      //   [-73.997586, 40.713448],
-      //   [-73.99256, 40.713863]
-      // ],
-      path: GeoData.default[0].legs[0].steps[2].intersections.map(
-        x => x.location
-      ),
+      path: props.points,
       name: "Test Data",
       color: [0, 153, 255]
     }
@@ -40,8 +17,8 @@ function Mapbox(props) {
 
   const [state, setState] = React.useState({
     viewport: {
-      width: 600,
-      height: 400,
+      width: "100%",
+      height: 700,
       latitude: 52.3667,
       longitude: 4.8945,
       zoom: 13
@@ -54,9 +31,10 @@ function Mapbox(props) {
     pickable: true,
     widthScale: 20,
     widthMinPixels: 2,
+    rounded: true,
     getPath: d => d.path,
     getColor: d => d.color,
-    getWidth: d => 0.01
+    getWidth: d => 0.05
   });
 
   return (
@@ -66,6 +44,8 @@ function Mapbox(props) {
       layers={[layer]}
     >
       <ReactMapGL
+        width={400}
+        height={400}
         {...state.viewport}
         onViewportChange={viewport => setState({ viewport })}
         mapboxApiAccessToken={
@@ -75,5 +55,9 @@ function Mapbox(props) {
     </DeckGL>
   );
 }
+
+Mapbox.defaultProps = {
+  points: GeoData.default[0].legs[0].steps[2].intersections.map(x => x.location)
+};
 
 export default Mapbox;
